a0ba042ec297c07657463602d8ff6000b93d5f1d
Zach <zach.johnson070@gmail.com>
2015-04-30 17:27:55 -0600

    charsheet link

--- dnd/dndTemplate.php
+++ dnd/dndTemplate.php
@@ -22,7 +22,7 @@
 							<div id="back" class ="link"><p class="link"><a class="link" href='../lols.php'>Back to Lols</a></p></div>
 							<div id='link1' class="link"><p class="link"><a class="link" href='index.php'>DnD Home</a></p></div>
 							<div id='link2' class="link"><p class="link"><a class="link" href='docs/index.php'>Docs</a></p></div>
-							<div id='link3' class="link"><p class="link"><a class="charsheetlink" href='charsheet/Charsheet.php'>Character<br/>Sheet</a></p></div>
+							<div id='link3' class="link"><p class="charsheetlink"><a class="link" href='charsheet/Charsheet.php'>Character<br/>Sheet</a></p></div>
 						</div>
 						
 						<div id='content'>
