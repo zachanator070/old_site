fe4062a50ede1d3f87132c77d75c83b14722e0a0
Zach <zach.johnson070@gmail.com>
2015-05-10 16:08:46 -0600

    added pictures

---
+++ blog/mom.php
@@ -0,0 +1,160 @@
+<html>
+	<head>
+		<link rel = "stylesheet" type = "text/css" href = "blogstyle.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+		<script src="../da_script.js" language="javascript" type="text/javascript"></script>
+		
+	</head>
+	
+	<body onload="updateClock(); setInterval('updateClock()', 1000 )">
+				
+				
+		<div id="frame">	
+			<div id = "scroll">
+				<h1>Title</h1>	
+			</div>
+				
+			<div id ="anchors">
+					
+			</div>
+				
+			<div id="clock">
+			
+			</div>
+				
+			<div id = "content">
+				 <div class="entry">
+					 <p>
+					 So today is Mother's day, so naturally I wanted to dedicate this entry to the 
+					wonderful woman who birthed me some 23 years ago. I wanted to share some things
+					that I will always remember about my mom in hopes that everyone can perhaps learn
+					something from her as I have.
+					</p>
+					<p>
+					So my mom was always keeping us kids busy, especially when it was summer and there 
+					wasn't much to do. The classic situation that I fell into so many times was a 
+					conversation that went something like this:
+					</p>
+					<p>
+					me: Hey mom I want to do X.
+					mom: Well you haven't done A,B,C or D yet.
+					me: But mom that is going to take forever!!
+					mom: Well next time you should plan better.
+					</p>
+					<p>
+					So after this happened to me about 923879237 times I finally was able to 
+					find a work around. One day this conversation had happened again and she said that
+					I needed to stain the arbor before going out. So I called my friends and told them this
+					story. After rallying my friends together we had stained the huge wooden arbor in our
+					back yard in a couple of hours. After all was said and done she bought us lunch. I think it
+					was more of a tribute to my cunning ways to get out of chores, or perhaps it was because she
+					felt bad for my friends who fell for my plot. Either way I learned a great lesson that day:
+					If you want to go out with friends, tell your mom days in advance so she can't pull
+					last minute chores on you. 
+					</p>
+					<img align="center" width="600px" height="400px" src="pics/momajohnson.jpg" />
+					
+					<p>
+					The next fond memory I have of my mother was on a hot summer's day in Texas. Now, as a 
+					male teenager, I was subjected to many great and wonderful ideas that my friends and I
+					were always willing to try to pass the time. This day marked the birth of one of our
+					finest ideas, the red-neck jacuzzi.
+					</p>
+					<p>
+					This idea consisted of lining my truck with a tarp and then proceeding to fill the bed of the truck with water.
+					What geniuses we were. After rolling around in my neighborhood in a pickup truck filled with water, we pulled into
+					my driveway to be greeted by my mother and father. My mom was brimming with anger as she scolded us for using
+					so much water in such a silly endeavour while we had a swimming pool in the back yard that we could
+					have easily used instead. She then yelled "STEVE! TELL THESE BOYS HOW FOOLISH THIS WAS!" Dad responded with,
+					"Now boys, listen to your mother" like any good father would. After after my mom stormed inside dad then asked "So how
+					was it?" "Awesome" we replied. He smiled and gave us a nod of approval, then told us to poor the water on the trees outside dying
+					from the Texas heat. It was a cherished memory where I learned that sometimes mom just doesn't appreciate your 
+					brilliant ideas and that next time you should probably invite dad to come goof around. 
+					</p>
+					<p>
+					So now that I have thoroughly embarrassed mom, I wanted to share one of the more powerful lessons in my life.
+					It was a rather troubling time in my life as I was headed off to school and had to be making some life choices.
+					I had thought long and hard but still couldn't see a way out of my problems. I then confronted by parents about
+					my dilemma to see if their life experience would give me some inspiration. My mom was the one that had spoken up
+					when asked what to do and she gave me the hardest answer of my life when she said "Zach, we have raised you in the
+					gospel and have taught you everything we know about it. It is up to you to decide for yourself how to apply
+					the principles that we have taught you." So she pretty much didn't answer my question which initially made me kind
+					of frustrated but, later on I saw the light. 
+					<p>
+					</p>
+					My mom has taught me that being a parent means to teach but not to force things upon others. It was her responsibility
+					to get me to a point to decide on my own. This taught me a great skill that I needed very early in my life which was 
+					the ability to act on my own out of my own will. I can imagine how scary it is to invest so much love, time, and patience
+					into a child and then suddenly have them acting and choosing on their own. It is something that I will credit to my
+					parents who have taught me what to live for and how. 
+					</p>
+					<p>
+					Thank you so much mom for all that you do and for the wonderful life that you have prepared me for. One day I hope 
+					that I can make it up to you whether it be in this life or the next.
+					</p>
+					<p>
+					Now having been married I have officially been adopted into another family. Now I don't have any cool stories about
+					Alyssum's awesome mom Grace Teh, but I asked Alyssum what her favorite memory of her mom was. She said that 
+					"My mother is very generous and giving. She has always been less concerned about how much we have and more on what 
+					we could give to others. I remember when she asked us kids to bring food almost everyday to an elderly couple in our
+					 neighborhood in the Philippines. They lived in a little shack and the husband fell very ill and eventually died. 
+					 We continued giving food to his wife until we had to leave for the US. I wasn't always an enthusiastic little delivery 
+					 girl because I always got interrupted from my personal pursuits. But now I've come to appreciate the experience. 
+					 I learned a lot about service, having a grateful heart and seeing others as Christ sees them.  I don't think I can be
+					 near as charitable as I want to be without a mother who's Christ-like and provides opportunities for me to learn and
+					 become more like Him."
+					 </p>
+					 <p>
+					Thank you so much nanay Teh for raising a wonderful young woman and for the great example that you have given to all of
+					us. I am so happy to be apart of the Teh family now and can't wait till we have some great stories to tell!
+					</p>
+					
+					<img align="center" width="400px" height="600px" src="pics/momateh.jpg" />
+					
+					<p>
+					For all you other people out there, take the time to say something to your mom today and show her your appreciation, it
+					will do you both good. :)
+					</p>
+				 </div>
+				
+			</div>
+			
+			<div id = "nav">
+				<a href="../index.php" class="navlink">
+				<div id="navmid">
+					Home
+				</div>
+				</a>
+				
+				<a href="index.php" class="navlink">
+				<div id="navmid">
+					Blog
+				</div>
+				</a>
+				
+				<a href="../quotes.php" class="navlink">
+				<div id="navmid">
+					Quotes
+				</div>
+				</a>
+				
+				<a href="../lols.php" class="navlink">
+				<div id="navmid">
+					Lols
+				</div>
+				</a>
+			</div>
+			
+			<div id="entries">
+					<h3 class="entryTitle">Entries</h3>
+					<ul>
+						<?php
+						include "entries.php";
+						
+						?>
+					</ul>
+			</div>
+			
+		</div>
+	</body>
+</html>
