e938e916d4b67fa4e92e7438eac975eaf305011a
Zach <zach.johnson070@gmail.com>
2015-10-08 21:30:21 -0600

    trying facebook

---
+++ test/FacebookLogin.php
@@ -0,0 +1,14 @@
+require_once __DIR__ . '/vendor/autoload.php';
+
+# login.php
+$fb = new Facebook\Facebook([
+  'app_id' => '1642711959335354',
+  'app_secret' => '6de714ca6cb3358987a6383853a3a235',
+  'default_graph_version' => 'v2.2',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['email', 'user_likes']; // optional
+$loginUrl = $helper->getLoginUrl('http://thezachcave.com/test/login-callback.php', $permissions);
+
+echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
---
+++ test/login-callback.php
@@ -0,0 +1,44 @@
+require_once __DIR__ . '/vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => '1642711959335354',
+  'app_secret' => '6de714ca6cb3358987a6383853a3a235',
+  'default_graph_version' => 'v2.2',
+  ]);
+
+
+//get access token
+$helper = $fb->getCanvasHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+  exit;
+}
+
+//grab user information
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name', $accessToken);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'Name: ' . $user['name'];
