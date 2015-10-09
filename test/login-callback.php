84349d23866dd24a3c7f83cb4d63de32332d6a41
Zach <zach.johnson070@gmail.com>
2015-10-08 22:01:53 -0600

    trying facebook

--- test/login-callback.php
+++ test/login-callback.php
@@ -12,28 +12,6 @@ $fb = new Facebook\Facebook([
 
 
 //get access token
-$helper = $fb->getCanvasHelper();
-
-$accessToken;
-
-try {
-  $accessToken = $helper->getAccessToken();
-
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-
-$fb = new Facebook\Facebook([
-  'app_id' => '1642711959335354',
-  'app_secret' => '{your-app-secret}',
-  'default_graph_version' => 'v2.4',
-  ]);
 
 $helper = $fb->getRedirectLoginHelper();
 
