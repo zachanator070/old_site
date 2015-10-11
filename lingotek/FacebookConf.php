c388a09583146b40c4c563ec5d07818911ef8d64
Zach <zach.johnson070@gmail.com>
2015-10-10 18:30:33 -0600

    oops now i did it

---
+++ lingotek/FacebookConf.php
@@ -0,0 +1,12 @@
+
+<?php
+
+  //these are the default global values used for our app to connect to
+  //the Facebook SDK
+
+    $config = array();
+
+    $config['app_id'] ='1642711959335354';
+    $config['app_secret'] = '6de714ca6cb3358987a6383853a3a235';
+
+ ?>
